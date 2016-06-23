6d06d4cc5e146b825c2ab021964c31e3fc4831a8
Hunter MacDonald <mactinite77@gmail.com>
2016-06-22 19:29:31 -0700

    initial push of codebase

---
+++ src/UserData.java
@@ -0,0 +1,35 @@
+/*
+ * UserData Bean
+ * by Hunter MacDonald
+ */
+public class UserData {
+	private String username;
+	private String email;
+	private String password;
+	private String Salt;
+	
+	public String getSalt() {
+		return Salt;
+	}
+	public void setSalt(String salt) {
+		Salt = salt;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String pwd) {
+		this.password = pwd;
+	}	
+}
